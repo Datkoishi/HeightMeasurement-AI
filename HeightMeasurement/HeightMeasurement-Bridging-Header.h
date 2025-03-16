43f0b385e6e625cad046638c0dce555f4118a0df
datkoi211 <truongdinhdetrel@gmail.com>
2025-03-16 11:17:50 +0700

    Initial Commit

---
+++ HeightMeasurement/HeightMeasurement-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  HeightMeasurement-Bridging-Header.h
+//  HeightMeasurement
+//
+//  Created by Trương Đạt on 16/3/25.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
---
+++ HeightMeasurementTests/HeightMeasurementTests.swift
@@ -0,0 +1,17 @@
+//
+//  HeightMeasurementTests.swift
+//  HeightMeasurementTests
+//
+//  Created by Trương Đạt on 16/3/25.
+//
+
+import Testing
+@testable import HeightMeasurement
+
+struct HeightMeasurementTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
